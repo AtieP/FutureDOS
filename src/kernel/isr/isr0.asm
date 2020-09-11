89af38e4396ca4c84e151e9eaad9921c841297f5
AtieP <nosequiensoy8664@gmail.com>
2020-09-11 18:12:27 +0200

    Add what interrupt is the division by zero in the error message

--- src/kernel/isr/isr0.asm
+++ src/kernel/isr/isr0.asm
@@ -30,7 +30,7 @@ isr0:
 
 .ERROR_MESSAGE_HEADER_STR:
     db 0x0A,0x0D
-    db "Unhandled division by zero exception."
+    db "Unhandled division by zero exception (int 0x00)."
     db 0x00
 .ERROR_MESSAGE_BODY_STR: db 0x0A,0x0D,"Register dump:",0x0A,0x0D,0x00
 .ERROR_MESSAGE_FOOTER_STR: db 0x0A,0x0D,"Terminating application.",0x00
