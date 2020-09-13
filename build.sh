fd52f952bb983d969b5afb42e2416553981e3aff
AtieP <nosequiensoy8664@gmail.com>
2020-09-13 18:29:26 +0200

    Raise error code 1 when there is an error while assemblying

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 mkdir -p bin
 rm bin/*
 cd src
-nasm -f bin boot/boot.asm -o ../bin/boot
-nasm -f bin kernel/kernel.asm -o ../bin/kernel
+nasm -f bin boot/boot.asm -o ../bin/boot || exit 1
+nasm -f bin kernel/kernel.asm -o ../bin/kernel || exit 1
 cd ..
 cat bin/kernel >> bin/boot
 mkdir -p disk
