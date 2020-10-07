4e6b7f1ddb44d46990eceb3642c1e9fdc03ea509
AtieP <AtieP@users.noreply.github.com>
2020-10-07 20:22:29 +0200

    Add keyboard input and echo and reset commands

--- apps/terminal.asm
+++ apps/terminal.asm
@@ -1,16 +1,124 @@
 bits 16
 cpu 8086
 
-push cs
-pop ds
+main:
+    push cs
+    pop ds
+    push cs
+    pop es
 
-mov si, _HELLO_FROM_TERMINAL_BIN
-mov bl, 0x0E
-mov ah, 0x06
-int 22h
+.read_loop:
+    call clear_buffer
 
-hang:
-    hlt
-    jmp hang
+    ; Print a new line
+    mov ax, (0x05 << 8) | 0x0A
+    mov bl, [DATA.NORMAL_COLOR]
+    int 22h
 
-_HELLO_FROM_TERMINAL_BIN: db "Hello from TERMINAL.BIN!",0x00
+    mov al, 0x0D
+    int 22h
+
+    ; Print prompt
+    mov ah, 0x06
+    mov si, DATA.PROMPT_STR
+    int 22h
+
+    ; Read input
+    mov ah, 0x04
+    mov di, DATA.BUFFER
+    mov cx, DATA.BUFFER.LEN
+    int 22h
+
+    ; Check if the input is internal commands
+    mov si, DATA.COMMANDS.ECHO
+    mov cx, DATA.COMMANDS.ECHO.LEN
+    call str_startswith
+    jnc .echo
+
+    mov si, DATA.COMMANDS.RESET
+    mov cx, DATA.COMMANDS.RESET.LEN
+    call str_startswith
+    jnc .reset
+
+    ; Check if input is a file
+    mov ah, 0x06
+    mov si, DATA.ERROR_MESSAGE
+    mov bl, [DATA.ERROR_COLOR]
+    int 22h
+
+    jmp .read_loop
+
+.echo:
+    mov si, DATA.BUFFER
+    add si, DATA.COMMANDS.ECHO.LEN
+    inc si
+
+    mov ah, 0x06
+    int 22h
+    jmp .read_loop
+
+.reset:
+    int 19h
+
+DATA:
+.PROMPT_STR: db "</> ",0x00
+.NORMAL_COLOR: db 0x0F
+.ERROR_COLOR: db 0x04
+.BUFFER: times 127 db 0x00
+.BUFFER.LEN: equ $ - .BUFFER - 1 ; Substract the NULL end byte
+.ERROR_MESSAGE: db "Invalid command provided",0x00
+
+.COMMANDS:
+.COMMANDS.ECHO: db "echo"
+.COMMANDS.ECHO.LEN: equ $ - .COMMANDS.ECHO
+.COMMANDS.RESET: db "reset"
+.COMMANDS.RESET.LEN: equ $ - .COMMANDS.RESET
+
+.FILE: db "TERMINALBIN"
+
+; Clears the command buffer.
+; IN/OUT: Nothing
+clear_buffer:
+    xor al, al
+    mov cx, DATA.BUFFER.LEN
+    mov di, DATA.BUFFER
+    rep stosb
+    ret
+
+; Check if the string has the prefix.
+; IN: DI = Original string, SI = Prefix, CX = Length of prefix
+; OUT: Carry set if not
+str_startswith:
+    push ax
+    push cx
+    push si
+    push di
+    pushf
+
+.check:
+    mov al, [di]
+    cmp al, [si]
+    jne .mismatch
+
+    dec cx
+    jz .match
+
+    inc si
+    inc di
+    jmp .check
+
+.mismatch:
+    popf
+    stc
+    jmp .end
+
+.match:
+    popf
+    clc
+
+.end:
+    pop di
+    pop si
+    pop cx
+    pop ax
+    ret
