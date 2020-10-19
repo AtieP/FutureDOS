bd489c3c845a34a600a37cdd5f975ec132795caa
midn1 <7090833+midn1@users.noreply.github.com>
2020-10-19 15:30:47 +0300

    loadmz now supports programs with high amount of relocation entries

--- apps/loadmz.asm
+++ apps/loadmz.asm
@@ -2,38 +2,44 @@ org 0
 bits 16
 cpu 8086
 
-EXE_START_SEGMENT equ 0x1923
-PROGRAM_START_SEGMENT equ (EXE_START_SEGMENT + 3)
+EXE_START_SEGMENT equ (0x1900 + (LOADMZ_END - LOADMZ_START + 15) / 16)
+
+LOADMZ_START:
 
 ; Primitive MZ loader. Does not handle EXE files larger than 64kb.
 load_mz:
-	mov ax, cs
 	add si, 12 ;ds:si at first points to the command buffer. We pass the argument which comes 12 chars later into the 0xFD syscall
 	mov bx, EXE_START_SEGMENT
 	mov es, bx
 	xor bx, bx
 	mov ah, 0x07
 	int 0xFD
+	
 	jc .errRead
 	cmp word [es:bx], 0x5A4D
 	jne .errMagic
 	mov ax, [es:bx + 0x14]
 	mov [cs:MZ_JUMP_IP], ax
 	mov ax, [es:bx + 0x16]
-	add ax, PROGRAM_START_SEGMENT
+	add ax, EXE_START_SEGMENT
+	add ax, [es:bx + 0x08]
 	mov [cs:MZ_JUMP_SG], ax
 	mov sp, [es:bx + 0x10]
 	mov ax, [es:bx + 0x0E]
-	add ax, PROGRAM_START_SEGMENT
+	add ax, EXE_START_SEGMENT
+	add ax, [es:bx + 0x08]
 	mov ss, ax
 	mov cx, [es:bx + 0x06]
 	mov bp, [es:bx + 0x18]
 .relocationLoop:
 	mov ax, [es:bp + 2]
-	add ax, PROGRAM_START_SEGMENT
+	add ax, EXE_START_SEGMENT
+	add ax, [es:bx + 0x08]
 	mov ds, ax
+	mov ax, [es:bx + 0x08]
 	mov si, [es:bp + 0]
-	add word [ds:si], PROGRAM_START_SEGMENT
+	add word [ds:si], EXE_START_SEGMENT
+	add word [ds:si], ax
 	add bp, 4
 	loop .relocationLoop
 MZ_JUMP_IP equ ($ + 1)
@@ -54,3 +60,5 @@ MZ_JUMP_SG equ ($ + 3)
 	
 MSG_INVALID_MAGIC: db "Invalid MZ signature.", 0
 MSG_READ_FAIL: db "Failed to read file.", 0
+
+LOADMZ_END:
